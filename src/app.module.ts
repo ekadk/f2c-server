1aa213ad11b3333d0880bbd9d9a3291228e22c65
ekadk <ekadharmakusuma3@gmail.com>
2023-04-09 22:34:35 +0700

    [add]: nestjs config module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,10 @@
 import { Module } from '@nestjs/common';
 import { PrismaModule } from './prisma/prisma.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [ConfigModule.forRoot({
+    isGlobal: true
+  }), PrismaModule],
 })
 export class AppModule {}
