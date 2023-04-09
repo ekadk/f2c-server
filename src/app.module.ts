1543cecedfcffc41be04e02fc6c8289f1a857581
ekadk <ekadharmakusuma3@gmail.com>
2023-04-09 23:00:46 +0700

    [feat] auth: init

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { PrismaModule } from './prisma/prisma.module';
 import { ConfigModule } from '@nestjs/config';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [ConfigModule.forRoot({
     isGlobal: true
-  }), PrismaModule],
+  }), PrismaModule, AuthModule],
 })
 export class AppModule {}
