520213992874184ffefdca80b5c770b3069ba4ac
ekadk <ekadharmakusuma3@gmail.com>
2023-04-10 00:00:02 +0700

    [add]: basic signup functionality (test)

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,13 +1,14 @@
-import { Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { AuthDto } from './dto/auth.dto';
 
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
 
   @Post('local/signup')
-  signupLocal() {
-    return this.authService.signupLocal()
+  signupLocal(@Body() dto: AuthDto) {
+    return this.authService.signupLocal(dto)
   }
 
   @Post('local/signin')
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,12 +1,32 @@
-import { Injectable } from '@nestjs/common';
-
+import { ForbiddenException, Injectable } from '@nestjs/common';
+import { AuthDto } from './dto/auth.dto';
+import { PrismaService } from 'src/prisma/prisma.service';
+import * as argon from 'argon2';
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime/library';
 @Injectable()
 export class AuthService {
-  constructor() {}
-  async signupLocal() {
-    return {
-      message: 'from signup-local',
-    };
+  constructor(private prismaService: PrismaService) {}
+
+  async signupLocal(dto: AuthDto) {
+    const hash = await argon.hash(dto.password);
+
+    try {
+      const user = await this.prismaService.user.create({
+        data: {
+          email: dto.email,
+          password: hash,
+        },
+      });
+
+      return user;
+    } catch (error) {
+      if (error instanceof PrismaClientKnownRequestError) {
+        if (error.code === 'P2002') {
+          throw new ForbiddenException('credentials taken');
+        }
+      }
+      return error;
+    }
   }
 
   async siginLocal() {
