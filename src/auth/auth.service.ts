6a7ed3ebbaf7b9238332a31c2fd8cc7aa6518e03
ekadk <ekadharmakusuma3@gmail.com>
2023-04-10 02:12:54 +0700

    [feat]: signup & [add]: auth helpers

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -3,14 +3,27 @@ import { AuthDto } from './dto/auth.dto';
 import { PrismaService } from 'src/prisma/prisma.service';
 import * as argon from 'argon2';
 import { PrismaClientKnownRequestError } from '@prisma/client/runtime/library';
+import { JwtService } from '@nestjs/jwt';
+import { ConfigService } from '@nestjs/config';
+import { Tokens } from './types/tokens.type';
+
 @Injectable()
 export class AuthService {
-  constructor(private prismaService: PrismaService) {}
+  constructor(
+    private prismaService: PrismaService,
+    private configService: ConfigService,
+    private jwtService: JwtService,
+  ) {}
 
-  async signupLocal(dto: AuthDto) {
-    const hash = await argon.hash(dto.password);
+  // --------------
+  // MAIN METHODS |
+  // --------------
+
+  async signupLocal(dto: AuthDto): Promise<Tokens> {
+    const hash = await this.hashData(dto.password);
 
     try {
+      // creating user in DB
       const user = await this.prismaService.user.create({
         data: {
           email: dto.email,
@@ -18,7 +31,16 @@ export class AuthService {
         },
       });
 
-      return user;
+      // create tokens
+      const tokens = await this.getTokens(
+        user.id,
+        user.email,
+        user.licenseExpiredDate,
+      );
+
+      // update user's RT hash
+      await this.updateRtHash(user.id, tokens.rt);
+      return tokens;
     } catch (error) {
       if (error instanceof PrismaClientKnownRequestError) {
         if (error.code === 'P2002') {
@@ -45,5 +67,53 @@ export class AuthService {
     return {
       message: 'from refresh token',
     };
+  }
+
+  // ----------------
+  // HELPER METHODS |
+  // ----------------
+
+  async hashData(data: string) {
+    return argon.hash(data);
+  }
+
+  async getTokens(
+    userId: string,
+    email: string,
+    licenseExp: Date,
+    licenseKeyHash: string = '',
+  ) {
+    const payload = {
+      sub: userId,
+      email,
+      licenseExp,
+      licenseKeyHash,
+    };
+
+    const [at, rt] = await Promise.all([
+      this.jwtService.signAsync(payload, {
+        secret: this.configService.get('ACCESS_TOKEN_SECRET'),
+        expiresIn: 60 * 15,
+      }),
+      this.jwtService.signAsync(payload, {
+        secret: this.configService.get('ACCESS_TOKEN_SECRET'),
+        expiresIn: 60 * 60 * 24 * 7,
+      }),
+    ]);
+
+    return { at, rt };
+  }
+
+  async updateRtHash(userId: string, rt: string) {
+    try {
+      const rtHash = await this.hashData(rt);
+      await this.prismaService.user.update({
+        where: { id: userId },
+        data: { rtHash },
+      });
+      return;
+    } catch (error) {
+      return error;
+    }
   }
 }
