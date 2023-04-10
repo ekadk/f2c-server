6f367a05be85b934caa0ecb8891ffe7cdfff1c02
ekadk <ekadharmakusuma3@gmail.com>
2023-04-10 12:29:12 +0700

    [update] RT strategy: inject prisma service

--- src/auth/strategy/rt.strategy.ts
+++ src/auth/strategy/rt.strategy.ts
@@ -1,12 +1,16 @@
-import { Injectable } from '@nestjs/common';
+import { ForbiddenException, Injectable } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
 import { Request } from 'express';
 import { ExtractJwt, Strategy } from 'passport-jwt';
 import { ConfigService } from '@nestjs/config';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
 export class RefreshTokenStrategy extends PassportStrategy(Strategy, 'rt-jwt') {
-  constructor(configService: ConfigService) {
+  constructor(
+    configService: ConfigService,
+    private prismaService: PrismaService,
+  ) {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       secretOrKey: configService.get('REFRESH_TOKEN_SECRET'),
@@ -15,6 +19,11 @@ export class RefreshTokenStrategy extends PassportStrategy(Strategy, 'rt-jwt') {
   }
 
   validate(req: Request, payload: any) {
+    const user = this.prismaService.user.findUnique({
+      where: { id: payload.sub },
+    });
+    if (!user) throw new ForbiddenException('access denied');
+
     const refresh_token = req.get('authorization').replace('Bearer', '').trim();
     return {
       ...payload,
