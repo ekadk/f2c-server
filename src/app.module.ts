ecdc57592e6aeea3681aa9c0f2f4922b857bcd0e
ekadk <ekadharmakusuma3@gmail.com>
2023-04-10 12:25:10 +0700

    [update]: apply access_token guard globally

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,22 @@ import { Module } from '@nestjs/common';
 import { PrismaModule } from './prisma/prisma.module';
 import { ConfigModule } from '@nestjs/config';
 import { AuthModule } from './auth/auth.module';
+import { APP_GUARD } from '@nestjs/core';
+import { AccessTokenGuard } from './auth/guards/at.guard';
 
 @Module({
-  imports: [ConfigModule.forRoot({
-    isGlobal: true
-  }), PrismaModule, AuthModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    PrismaModule,
+    AuthModule,
+  ],
+  providers: [
+    {
+      provide: APP_GUARD,
+      useClass: AccessTokenGuard,
+    },
+  ],
 })
 export class AppModule {}
