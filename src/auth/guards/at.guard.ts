1810243409e9d111e107117cccd56221401f594e
ekadk <ekadharmakusuma3@gmail.com>
2023-04-10 12:31:33 +0700

    [update]: implement isPublic exception

--- src/auth/guards/at.guard.ts
+++ src/auth/guards/at.guard.ts
@@ -1,7 +1,23 @@
 import { AuthGuard } from '@nestjs/passport';
+import { Reflector } from '@nestjs/core';
+import { ExecutionContext, Injectable } from '@nestjs/common';
+import { Observable } from 'rxjs';
 
+@Injectable()
 export class AccessTokenGuard extends AuthGuard('at-jwt') {
-  constructor() {
+  constructor(private reflector: Reflector) {
     super({});
+  }
+
+  canActivate(
+    context: ExecutionContext,
+  ): boolean | Promise<boolean> | Observable<boolean> {
+    const isPublic = this.reflector.getAllAndOverride('isPublic', [
+      context.getHandler(),
+      context.getClass(),
+    ]);
+
+    if (isPublic) return true;
+    return super.canActivate(context);
   }
 }
