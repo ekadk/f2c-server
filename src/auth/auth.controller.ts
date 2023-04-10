e8c5a3f404ab86835a42abfdfd9e6ebc8c5f4360
ekadk <ekadharmakusuma3@gmail.com>
2023-04-10 14:20:52 +0700

    [update]: add public decorator to several auth routes

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,32 +1,33 @@
 import { Body, Controller, Post, Req, UseGuards } from '@nestjs/common';
 import { AuthService } from './auth.service';
-import { AuthGuard } from '@nestjs/passport';
 import { AuthDto } from './dto/auth.dto';
 import { Request } from 'express';
-import { AccessTokenGuard } from './guards/at.guard';
 import { RefreshTokenGuard } from './guards/rt.guard';
+import { Public } from 'src/common/decorator/public.decorator';
 
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
 
+  @Public()
   @Post('local/signup')
   signupLocal(@Body() dto: AuthDto) {
     return this.authService.signupLocal(dto);
   }
 
+  @Public()
   @Post('local/signin')
   siginLocal(@Body() dto: AuthDto) {
     return this.authService.siginLocal(dto);
   }
 
-  @UseGuards(AccessTokenGuard)
   @Post('local/logout')
   logout(@Req() req: Request) {
     const user = req.user;
     return this.authService.logout(user);
   }
 
+  @Public()
   @UseGuards(RefreshTokenGuard)
   @Post('local/refresh')
   refreshToken(@Req() req: Request) {
