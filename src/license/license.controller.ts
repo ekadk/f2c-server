e7f73a88c4aa8c547a9edd93b8dbe0b022533e25
ekadk <ekadharmakusuma3@gmail.com>
2023-04-10 23:25:35 +0700

    [update]: license routes

--- src/license/license.controller.ts
+++ src/license/license.controller.ts
@@ -1,34 +1,25 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+} from '@nestjs/common';
 import { LicenseService } from './license.service';
 import { CreateLicenseDto } from './dto/create-license.dto';
-import { UpdateLicenseDto } from './dto/update-license.dto';
+import { GetUserId } from 'src/common/decorator/get-user-id.decorator';
 
 @Controller('license')
 export class LicenseController {
   constructor(private readonly licenseService: LicenseService) {}
 
   @Post()
-  create(@Body() createLicenseDto: CreateLicenseDto) {
-    return this.licenseService.create(createLicenseDto);
-  }
-
-  @Get()
-  findAll() {
-    return this.licenseService.findAll();
-  }
-
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.licenseService.findOne(+id);
-  }
-
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateLicenseDto: UpdateLicenseDto) {
-    return this.licenseService.update(+id, updateLicenseDto);
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.licenseService.remove(+id);
+  create(
+    @GetUserId() userId: string,
+    @Body() createLicenseDto: CreateLicenseDto,
+  ) {
+    return this.licenseService.create(userId, createLicenseDto);
   }
 }
