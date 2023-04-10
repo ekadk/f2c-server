8820c6829df52c5de30193fb2fe88db3d5cb4b94
ekadk <ekadharmakusuma3@gmail.com>
2023-04-11 02:56:18 +0700

    [feat]: add license key to db

--- src/license/license.service.ts
+++ src/license/license.service.ts
@@ -1,26 +1,80 @@
-import { Injectable } from '@nestjs/common';
+import { ForbiddenException, Injectable } from '@nestjs/common';
 import { CreateLicenseDto } from './dto/create-license.dto';
-import { UpdateLicenseDto } from './dto/update-license.dto';
+import axios from 'axios';
+import * as argon from 'argon2';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class LicenseService {
-  create(createLicenseDto: CreateLicenseDto) {
-    return 'This action adds a new license';
+  constructor(
+    private prismaService: PrismaService,
+    configService: ConfigService,
+  ) {}
+
+  async create(userId: string, createLicenseDto: CreateLicenseDto) {
+    try {
+      const instance_name = await argon.hash(userId);
+      const data = await this.requestToLemonSqueezy(
+        createLicenseDto.license_key,
+        instance_name,
+      );
+      if (data.status !== 'active')
+        throw new ForbiddenException(`can't activate license`);
+
+      const expiredDate = this.getLicenseExpiredDate(data.createdAt);
+      const hashedKey = await argon.hash(createLicenseDto.license_key);
+
+      await this.prismaService.user.update({
+        where: { id: userId },
+        data: {
+          licenseKey: hashedKey,
+          licenseExpiredDate: expiredDate,
+        },
+      });
+
+      return {
+        message: 'license key added!',
+      };
+    } catch (error) {
+      return error;
+    }
   }
 
-  findAll() {
-    return `This action returns all license`;
+  async requestToLemonSqueezy(license_key: string, instance_name: string) {
+    const url = 'https://api.lemonsqueezy.com/v1/licenses/activate';
+    try {
+      const { data } = await axios.post(
+        url,
+        {
+          license_key,
+          instance_name,
+        },
+        {
+          headers: {
+            Accept: 'application/json',
+          },
+        },
+      );
+
+      const extracted = {
+        status: data.license_key.status,
+        createdAt: data.license_key.created_at,
+        instanceId: data.instance.id,
+        instanceName: data.instance.name,
+      };
+
+      return extracted;
+    } catch (error) {
+      throw error;
+    }
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} license`;
-  }
-
-  update(id: number, updateLicenseDto: UpdateLicenseDto) {
-    return `This action updates a #${id} license`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} license`;
+  getLicenseExpiredDate(createdDate) {
+    const formattedCreatedDate = new Date(createdDate);
+    const licenseExpiredDate = new Date(
+      formattedCreatedDate.setMonth(formattedCreatedDate.getMonth() + 1),
+    );
+    return licenseExpiredDate.toJSON();
   }
 }
